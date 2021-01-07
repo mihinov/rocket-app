3721bf5fb0672d169210fa3c256f62f06228901d
mihinov <kamahinmihail@gmail.com>
2021-01-07 19:49:16 +0300

    go

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { GraphQLModule } from './graphql.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 import { ShipsListComponent } from './ships-list/ships-list.component';
 import { ShipsPaginatorComponent } from './ships-list/ships-paginator/ships-paginator.component';
 import { LoaderComponent } from './shared/components/loader/loader.component';
 import { ShipsFilterComponent } from './ships-filter/ships-filter.component';
 import { PageShipsComponent } from './page-ships/page-ships.component';
 import { ShipDetailsComponent } from './ship-details/ship-details.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     GraphQLModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ships-filter/ships-filter.component.ts
+++ src/app/ships-filter/ships-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ships-filter',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShipsFilterComponent implements OnInit {
 
+  formFilter: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.formFilter = new FormGroup({
+      checkboxGroup: new FormGroup({
+       'checkbox-port-canaveral': new FormControl(true),
+       'checkbox-port-of-los-angeles': new FormControl(true),
+        'checkbox-fort-lauderdale': new FormControl(false)
+      })
+    });
+  }
 
+  onChangeCheckbox(): void {
+    console.log(Object.values(this.formFilter.controls.checkboxGroup.controls).map(item => item.value));
   }
 
 }
